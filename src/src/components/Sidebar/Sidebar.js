a6735dcf76cc147df0ae17299d54e90f70d26491
Unknownpgr <unknownpgr@gmail.com>
2021-07-05 19:07:38 +0900

    Improve sidebar animation

--- src/src/components/Sidebar/Sidebar.js
+++ src/src/components/Sidebar/Sidebar.js
@@ -4,12 +4,20 @@ import "./sidebar.css";
 export default function Sidebar({ n, c, onClick }) {
   let dots = [];
   for (let i = 0; i < n; i++) {
-    let padding = i === c ? "1rem" : "0.25rem";
+    let padding = i === c ? "1" : "0.25";
     let paddingTop = 0;
     let paddingBottom = 0;
 
-    if (i > 0) paddingTop = padding;
-    if (i < n - 1) paddingBottom = padding;
+    if (i === 0) {
+      paddingTop = 0 + "rem";
+      paddingBottom = padding * 2 + "rem";
+    } else if (i === n - 1) {
+      paddingTop = padding * 2 + "rem";
+      paddingBottom = 0 + "rem";
+    } else {
+      paddingTop = padding + "rem";
+      paddingBottom = padding + "rem";
+    }
 
     dots.push(
       <div
