69fb020bc94f01ca1ce3144da4c1f656a2bc9393
unknownpgr <unknownpgr@gmail.com>
2021-07-12 13:22:55 +0000

    Update opacity

--- src/src/components/Card/Card.js
+++ src/src/components/Card/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Card.css";
 
 export default function Card({ index, current }) {
-  let opacity = index === current ? '1' : '0.5'
+  let opacity = index === current ? '1' : '0.25'
   return (
     <div className="card" style={{ opacity }}>
       <h3>
